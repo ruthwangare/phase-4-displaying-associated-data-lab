e07bdcb3d25698a14f13b0f6d3943d67d22cd66b
Ruth <rwangare124@gmail.com>
2023-07-02 13:50:14 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items.to_json(include: { user: { only: [:id, :username, :city] } })
+      end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(include: :items)
+    end
+
+
 end
